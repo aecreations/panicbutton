1a1aa4a07ad156a8fbdb8ba0d66a84839b3c81e0
AE Creations <aecreations@server.fake>
2017-04-14 22:00:48 -0700

    Added option to change replacement URL

--- wx-src/options.js
+++ wx-src/options.js
@@ -21,6 +21,11 @@ function initOptions(aEvent)
     console.log(aResult);
     $("panicbutton-action").selectedIndex = aResult.action;
     $("shortcut-key").checked = aResult.shortcutKey;
+    $("webpg-url").value = aResult.replacementWebPgURL;
+
+    if (aResult.action == PANICBUTTON_ACTION_REPLACE) {
+      $("panicbutton-action-options-hide-and-replace").style.display = "block";
+    }
   }, onError);
 }
 
@@ -33,13 +38,33 @@ function saveOptions(aEvent)
   let aePanicButtonPrefs = {
     action: actionSelect.options[actionSelect.selectedIndex].value,
     shortcutKey: $("shortcut-key").checked,
-    replacementWebPgURL: REPLACE_WEB_PAGE_DEFAULT_URL
+    replacementWebPgURL: $("webpg-url").value
   };
     
   let setPrefs = browser.storage.local.set(aePanicButtonPrefs);
   setPrefs.then(() => {
     console.log("Panic Button/wx: Preferences saved.");
   }, onError);
+}
+
+
+function updatePanicButtonActionDesc(aEvent)
+{
+  let selectElt = aEvent.target;
+  let panicButtonAction = selectElt.options[selectElt.selectedIndex].value;
+  
+  if (panicButtonAction == PANICBUTTON_ACTION_REPLACE) {
+    $("panicbutton-action-options-hide-and-replace").style.display = "block";
+  }
+  else {
+    $("panicbutton-action-options-hide-and-replace").style.display = "none";
+  }
+}
+
+
+function resetWebPageURL()
+{
+  $("webpg-url").value = REPLACE_WEB_PAGE_DEFAULT_URL;
 }
 
 
@@ -51,5 +76,5 @@ function onError(aError)
 
 document.addEventListener("DOMContentLoaded", initOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
-
+document.querySelector("#panicbutton-action").addEventListener("change", updatePanicButtonActionDesc);
 

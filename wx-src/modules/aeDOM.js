77f9d713f70c1b3990d1d1d2bff21167c7521a15
AE Creations <aecreations@users.noreply.github.com>
2021-01-02 19:46:02 -0800

    Refactored the aeDOM module

--- wx-src/modules/aeDOM.js
+++ wx-src/modules/aeDOM.js
@@ -3,52 +3,47 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-let aeDOM = {};
+let aeDOM = {
+  createTextNode(aStringKey)
+  {
+    let rv = document.createTextNode(browser.i18n.getMessage(aStringKey));
+    return rv;
+  },
 
-aeDOM.createTextNode = function (aStringKey)
-{
-  let rv = document.createTextNode(browser.i18n.getMessage(aStringKey));
-  return rv;
+  createTextNodeWithSpc()
+  {
+    let rv = document.createTextNode("\u00a0");
+    return rv;
+  },
+
+  createEltWithID(aNodeName, aNodeID, aStringKey)
+  {
+    let rv = document.createElement(aNodeName);
+    rv.id = aNodeID;
+    let text = document.createTextNode(browser.i18n.getMessage(aStringKey));
+    rv.appendChild(text);
+
+    return rv;
+  },
+
+  createEltWithClass(aNodeName, aNodeClass, aStringKey)
+  {
+    let rv = document.createElement(aNodeName);
+    rv.className = aNodeClass;
+    let text = document.createTextNode(browser.i18n.getMessage(aStringKey));
+    rv.appendChild(text);
+
+    return rv;
+  },
+
+  createHyperlink(aStringKey, aURL)
+  {
+    let rv = document.createElement("a");
+    rv.className = "hyperlink";
+    rv.setAttribute("href", aURL);
+    let text = document.createTextNode(browser.i18n.getMessage(aStringKey));
+    rv.appendChild(text);
+
+    return rv; 
+  }
 };
-
-
-aeDOM.createTextNodeWithSpc = function ()
-{
-  let rv = document.createTextNode("\u00a0");
-  return rv;
-};
-
-
-aeDOM.createEltWithID = function (aNodeName, aNodeID, aStringKey)
-{
-  let rv = document.createElement(aNodeName);
-  rv.id = aNodeID;
-  let text = document.createTextNode(browser.i18n.getMessage(aStringKey));
-  rv.appendChild(text);
-
-  return rv;
-};
-
-
-aeDOM.createEltWithClass = function (aNodeName, aNodeClass, aStringKey)
-{
-  let rv = document.createElement(aNodeName);
-  rv.className = aNodeClass;
-  let text = document.createTextNode(browser.i18n.getMessage(aStringKey));
-  rv.appendChild(text);
-
-  return rv;
-};
-
-
-aeDOM.createHyperlink = function (aStringKey, aURL)
-{
-  let rv = document.createElement("a");
-  rv.className = "hyperlink";
-  rv.setAttribute("href", aURL);
-  let text = document.createTextNode(browser.i18n.getMessage(aStringKey));
-  rv.appendChild(text);
-
-  return rv; 
-};
-
